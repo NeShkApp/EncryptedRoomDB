c75d51a63a93343cd5151db34ab481f212a3f550
bohdan-n-flp <bohdan.n@flipsidegroup.com>
2025-06-05 10:29:31 +0200

    migration to sqlcipher test

--- app/src/main/java/com/mun/bonecci/encryptedroomdb/db/UserDatabase.kt
+++ app/src/main/java/com/mun/bonecci/encryptedroomdb/db/UserDatabase.kt
@@ -6,9 +6,12 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
+import com.mun.bonecci.encryptedroomdb.data.cipher.PassPhraseUtils
+import com.mun.bonecci.encryptedroomdb.data.cipher.SQLCipherUtils
 import com.mun.bonecci.encryptedroomdb.data.models.Log
 import com.mun.bonecci.encryptedroomdb.data.models.Session
 import com.mun.bonecci.encryptedroomdb.data.models.User
+import net.sqlcipher.database.SupportFactory
 
 /**
  * Room database class representing the user database.
@@ -37,6 +40,7 @@ abstract class UserDatabase : RoomDatabase() {
      */
     companion object {
         @Volatile private var instance: UserDatabase? = null
+        private const val DATABASE_NAME = "user_database.db"
 
         val MIGRATION_1_2 = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
@@ -75,13 +79,29 @@ abstract class UserDatabase : RoomDatabase() {
          * @param context The application context.
          * @return The UserDatabase instance.
          */
+
         @Synchronized
         fun getInstance(context: Context): UserDatabase {
+            // Get the user passphrase and convert it to a byte array
+            val userPassphrase = PassPhraseUtils.getPassphrase(context)
+            val passphrase = userPassphrase.toByteArray()
+
+            // Check the state of the database encryption
+            val state = SQLCipherUtils.getDatabaseState(context, DATABASE_NAME)
+
+            // Create a SupportFactory using the passphrase
+            val factory = SupportFactory(passphrase)
+
+            // Migrate the database to an encrypted one if it is currently unencrypted
+            if (state == SQLCipherUtils.State.UNENCRYPTED) {
+                SQLCipherUtils.migrateToEncryptedDatabase(DATABASE_NAME, context, userPassphrase)
+            }
             return instance ?: synchronized(this) {
                 val db = Room.databaseBuilder(
                     context.applicationContext, UserDatabase::class.java,
                     "user_database"
                 ).addMigrations(MIGRATION_1_2, MIGRATION_2_3)
+                    .openHelperFactory(factory)
                     .build()
                 instance = db
                 db
